b53991e5f5fb9552c7668b1cbf43f4bcd63e1a40
Pratham <pratham.sethi@mitratech.com>
2023-08-03 17:47:46 +0530

    added base to the config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pratham-webdev.github.io/astro-fun',
+    site: 'https://pratham-webdev.github.io',
+    base:'/astro-fun'
   })
